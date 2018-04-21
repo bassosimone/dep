45b89023de9910c2afc60a2974f66910fa315e05
Simone Basso <bassosimone@gmail.com>
2018-04-21 19:52:04 +0200

    Improve windows-amd64 script

--- script/build-windows-amd64.bat
+++ script/build-windows-amd64.bat
@@ -1,2 +1,5 @@
 @echo off
 cmake.exe "-GNMake Makefiles" -DCMAKE_BUILD_TYPE=Release "-DCMAKE_INSTALL_PREFIX=MK_DIST/windows/amd64" -DMK_DEP_WINDOWS_FORCE_CRT_MT=ON -DMK_DEP_DEPNAME=%1
+if %errorlevel% neq 0 exit /b %errorlevel%
+nmake.exe
+if %errorlevel% neq 0 exit /b %errorlevel%
